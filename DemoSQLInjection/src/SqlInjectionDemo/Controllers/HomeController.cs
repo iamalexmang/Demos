d236f0f2889d94c88fdefc0cc6f755e4fca282e7
amang <amang@LAPRND101>
2016-09-22 16:41:46 +0300

    Demo SQL Injection @ CloudBurst 2016

---
+++ DemoSQLInjection/src/SqlInjectionDemo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SqlInjectionDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            var loginSuccessful = false;
+            using (var sqlConnection = new SqlConnection("Server=tcp:cloudburstdbsrv.database.windows.net,1433;Database=demoinject;User ID=alex@cloudburstdbsrv;Password=123!@#qweQWE;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                sqlConnection.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    sqlCmd.Connection = sqlConnection;
+                    sqlCmd.CommandText = $"SELECT * FROM dbo.IUsers WHERE [Username] = '{username}' AND [Password] = '{password}'";
+                    using (var sqlDr = sqlCmd.ExecuteReader())
+                    {
+                        if (sqlDr.Read())
+                            loginSuccessful = true;
+                    }
+                }
+            }
+            if (loginSuccessful)
+                return RedirectToAction("Index", "Customers");
+            else
+                return View();
+        }
+    }
+}
